ed6f02780b52579db5a3bd0713ebb98f88cc6cd2
jai8151865z <jai8151865z@gmail.com>
2015-10-22 18:33:23 +0800

    q4:create recursive version

--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,68 @@
 /* FIXME: Implement! */
 
+#include <stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+
+int max(int a, int b);
+
+int max2(int a, int b, int c);
+
+int maxCrossingSum(int arr[], int l, int m, int h);
+
+int maxSubArraySum(int arr[], int l, int h);
+
 int main()
 {
-    return 0;
+   int arr[] = {10,-2,15,9,-8,12,20,-5};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   int max_sum = maxSubArraySum(arr, 0, n-1);
+   printf("Maximum contiguous sum is %d\n", max_sum);
+   return 0;
+}
+
+int max(int a, int b)
+{ 
+       return (a > b)? a : b; 
+}
+
+int max2(int a, int b, int c) 
+{
+        return max(max(a, b), c); 
+}
+
+int maxCrossingSum(int arr[], int l, int m, int h)
+{
+    
+    int sum = 0;
+    int left_sum = INT_MIN;
+    for (int i = m; i >= l; i--)
+    {
+        sum = sum + arr[i];
+        if (sum > left_sum)
+          left_sum = sum;
+     }
+
+    sum = 0;
+    int right_sum = INT_MIN;
+    for (int i = m+1; i <= h; i++)
+    {
+        sum = sum + arr[i];
+        if (sum > right_sum)
+          right_sum = sum;
+     }
+
+    return left_sum + right_sum;
+}
+
+int maxSubArraySum(int arr[], int l, int h)
+{
+   if (l == h)
+     return arr[l];
+
+   int m = (l + h)/2;
+
+   return max2(maxSubArraySum(arr, l, m),
+              maxSubArraySum(arr, m+1, h),
+              maxCrossingSum(arr, l, m, h));
 }
