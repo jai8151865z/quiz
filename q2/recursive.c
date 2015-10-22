35d816a191fa5598a7ef19011b0baf535ce94739
jai8151865z <jai8151865z@gmail.com>
2015-10-22 18:26:56 +0800

    q2:create recursive version

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,37 @@
 /* FIXME: Implement! */
 
+#include<stdio.h>
+#include<stdlib.h>
+
+char smallest_character(char str[],char c,int left,int right,char ret);
+
 int main()
 {
+    char str[]={'c','f','j','p','v'};
+    char c[]={'a','c','z','f'};
+    int left=0,right=(sizeof(str)/sizeof(str[0]))-1; 
+    char ret=str[0];
+    for(int i=0;i<(sizeof(c)/sizeof(c[0]));i++)
+   {
+         printf("output is %c\n",smallest_character(str,c[i],left,right,ret));
+    }
+
     return 0;
+}
+
+char smallest_character(char str[],char c,int left,int right,char ret)
+{
+    if(left<=right)
+    {
+         int m=left+((right-left)/2);
+         if(str[m]>c)
+         {
+              ret=str[m];
+              smallest_character(str,c,left,m-1,ret);
+          }
+         else
+              smallest_character(str,c,m+1,right,ret);
+     }
+     else
+           return ret;
 }
